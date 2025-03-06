4360bd0fb6fbf508a6131e464b4c2d81d711a1d3
Коновальчик Никита <swwork86@gmail.com>
2025-03-06 14:26:02 +0500

    feat: Добавлен ForEachAsync для IAsyncEnumerable

--- src/Shared/Core/Shared.Common/Extensions/LinqExtension.cs
+++ src/Shared/Core/Shared.Common/Extensions/LinqExtension.cs
@@ -12,7 +12,7 @@ namespace Shared.Common.Extensions;
 public static class LinqExtension
 {
     /// <summary>
-    /// <see cref="ForEach{T}"/> для <see cref="IEnumerable{T}"/>.
+    ///  <see cref="ForEach{T}"/> для <see cref="IEnumerable{T}"/>.
     /// </summary>
     /// <typeparam name="T">Тип перечисления.</typeparam>
     /// <param name="source">Перечисление.</param>
@@ -21,13 +21,13 @@ public static class LinqExtension
         this IEnumerable<T> source,
         Action<T> action)
     {
+        if (action == null)
+        {
+            throw new ArgumentNullException(nameof(action));
+        }
+
         foreach (var element in source)
         {
-            if (action == null)
-            {
-                throw new ArgumentNullException(nameof(action));
-            }
-
             action(element);
         }
     }
@@ -38,18 +38,47 @@ public static class LinqExtension
     /// <typeparam name="T">ип перечисления.</typeparam>
     /// <param name="source">Перечисление.</param>
     /// <param name="func">Операция.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
     /// <returns><see cref="Task"/>.</returns>
     public static async Task ForeachAsync<T>(
         this IEnumerable<T> source,
-        Func<T, Task> func)
+        Func<T, Task> func,
+        CancellationToken cancellationToken = default)
     {
+        if (func == null)
+        {
+            throw new ArgumentNullException(nameof(func));
+        }
+
         foreach (var item in source)
         {
-            if (func == null)
-            {
-                throw new ArgumentNullException(nameof(func));
-            }
+            cancellationToken.ThrowIfCancellationRequested();
+            await func(item);
+        }
+    }
 
+
+    /// <summary>
+    /// Асинхронная версия <see cref="ForEach{T}"/> для <see cref="IAsyncEnumerable{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">ип перечисления.</typeparam>
+    /// <param name="source">Перечисление.</param>
+    /// <param name="func">Операция.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns><see cref="Task"/>.</returns>
+    public static async Task ForEachAsync<T>(
+        this IAsyncEnumerable<T> source,
+        Func<T, Task> func,
+        CancellationToken cancellationToken = default)
+    {
+        if (func == null)
+        {
+            throw new ArgumentNullException(nameof(func));
+        }
+
+        await foreach (var item in source.WithCancellation(cancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
             await func(item);
         }
     }
