ff460ea5c5404ac8c52d0b76c5e6358d975442a4
a.felyugin <a.felyugin@inlinegroup.ru>
2025-10-03 08:07:12 +0000

    KNTR-10784 feat(outbox): implement HTTP Outbox + add migration

--- src/Services/DatabaseUpgrade/Gpn.Template.DatabaseUpgrade/Migrations/20251003075501_AddOutboxEvents.Designer.cs
+++ src/Services/DatabaseUpgrade/Gpn.Template.DatabaseUpgrade/Migrations/20251003075501_AddOutboxEvents.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Gpn.Template.Infrastructure.Dal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Gpn.Template.DatabaseUpgrade.Migrations
 {
     [DbContext(typeof(DbContext))]
-    partial class DbContextModelSnapshot : ModelSnapshot
+    [Migration("20251003075501_AddOutboxEvents")]
+    partial class AddOutboxEvents
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
