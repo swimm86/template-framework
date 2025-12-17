0206c714153bcf07d98d1613bbbc4b7df151cf08
Коновальчик Никита <swimm86@yandex.ru>
2025-12-17 13:53:56 +0300

    fix: Исправлено применение уровней логирования

--- src/Shared/Logging/Shared.Infrastructure.Logging/Extensions/NlogDependencyInjection.cs
+++ src/Shared/Logging/Shared.Infrastructure.Logging/Extensions/NlogDependencyInjection.cs
@@ -7,9 +7,13 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
 using NLog.Extensions.Logging;
 using Shared.Application.Core.Configuration.Extensions;
+using Shared.Common.Extensions;
 using Shared.Infrastructure.Logging.Settings;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace Shared.Infrastructure.Logging.Extensions;
 
@@ -28,17 +32,43 @@ public static class NlogDependencyInjection
         this IServiceCollection serviceCollection,
         IConfiguration configuration)
     {
+        var settings = configuration.GetOptions<NlogSettings>();
+        var configFileName = string.IsNullOrEmpty(settings?.Path) ? "nlog.base.config" : settings.Path;
+        var configPath = Path.IsPathRooted(configFileName)
+            ? configFileName
+            : Path.Combine(AppContext.BaseDirectory, configFileName);
+
+        // Загружаем конфигурацию NLog вручную, чтобы иметь возможность изменить правила
+        var nlogConfig = new XmlLoggingConfiguration(configPath);
+
+        // Определяем минимальный уровень логирования
+        var nlogLogLevel = settings?.LogLevel switch
+        {
+            LogLevel.Trace => NLog.LogLevel.Trace,
+            LogLevel.Debug => NLog.LogLevel.Debug,
+            LogLevel.Information => NLog.LogLevel.Info,
+            LogLevel.Warning => NLog.LogLevel.Warn,
+            LogLevel.Error => NLog.LogLevel.Error,
+            LogLevel.Critical => NLog.LogLevel.Fatal,
+            _ => NLog.LogLevel.Info
+        };
+
+        // Переопределяем минимальный уровень для всех правил логирования NLog
+        // Это необходимо, так как конфиг из NuGet может содержать неправильный уровень (Trace)
+        nlogConfig.LoggingRules.ForEach(rule =>
+            rule.SetLoggingLevels(nlogLogLevel, NLog.LogLevel.Fatal));
+
+        // Устанавливаем измененную конфигурацию в LogManager
+        LogManager.Configuration = nlogConfig;
+
         return serviceCollection
             .AddLogging(loggerBuilder =>
             {
-                var settings = configuration.GetOptions<NlogSettings>();
-                var configPath = settings?.Path;
                 loggerBuilder.ClearProviders();
-                loggerBuilder.AddNLog(string.IsNullOrEmpty(configPath) ? "nlog.base.config" : configPath);
+                loggerBuilder.AddNLog(nlogConfig);
+
                 if (settings is not null)
-                {
                     loggerBuilder.SetMinimumLevel(settings.LogLevel);
-                }
             });
     }
 }
