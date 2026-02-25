d2d3167e8512853783be19917120ef50160af28a
Коновальчик Никита <swimm86@yandex.ru>
2026-02-25 11:48:47 +0300

    fix(DalDependencyInjector): Добавлена упущенная регистрация DbSettingsBase

--- src/Services/Common/Gpn.Template.Infrastructure.Dal/DalDependencyInjector.cs
+++ src/Services/Common/Gpn.Template.Infrastructure.Dal/DalDependencyInjector.cs
@@ -4,8 +4,12 @@
 // </copyright>
 // ----------------------------------------------------------------------------------------------
 
+using Gpn.Template.Infrastructure.Dal.Settings;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Shared.Application.Core.Configuration.Extensions;
+using Shared.Application.Core.Dal.Settings.Models.Base;
 using Shared.Application.Core.DependencyInjection;
 
 namespace Gpn.Template.Infrastructure.Dal;
@@ -14,12 +18,18 @@ namespace Gpn.Template.Infrastructure.Dal;
 /// Внедрение зависимостей для DAL-слоя.
 /// </summary>
 public class DalDependencyInjector(
+    IConfiguration configuration,
     ILogger<DalDependencyInjector> logger)
     : DependencyInjectorBase(logger)
 {
     /// <inheritdoc />
     protected override IServiceCollection Process(IServiceCollection serviceCollection)
     {
-        return serviceCollection;
+        return serviceCollection
+            .AddSingleton<DbSettingsBase, DbSettings>(_ =>
+            {
+                var result = configuration.GetOptions<DbSettings>()!;
+                return result;
+            });
     }
 }
